0a442342b37c6cea01f9dd946f08eade98201739
ploiddddd <floydcayanan12@gmail.com>
2017-11-25 19:18:04 +0800

    routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {MenuDisplayComponent} from './menu-display/menu-display.component';
+import { UserIndexComponent } from './user-index/user-index.component';
+import {AppComponent} from './app.component';
+import { Component } from '@angular/core/src/metadata/directives';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { QrcodeComponent } from './qrcode/qrcode.component';
+
+const routes : Routes = [
+  {
+    path : 'menu-display',
+    component : MenuDisplayComponent
+  },
+  {
+    path : '',
+    component : UserIndexComponent
+  },
+  {
+    path : 'cart',
+    component : CartComponent
+  },
+  {
+    path : 'checkout',
+    component : CheckoutComponent
+  },
+  {
+    path : 'qrcode',
+    component : QrcodeComponent
+  }
+];
+
+@NgModule({
+  exports: [ RouterModule ],
+  imports: [ RouterModule.forRoot(routes) ],
+})
+
+export class AppRoutingModule { }
--- src/app/menu-display/menu-display.component.ts
+++ src/app/menu-display/menu-display.component.ts
@@ -6,21 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-display.component.css']
 })
 export class MenuDisplayComponent implements OnInit {
+  msg = 'BEVERAGES';
+  x = 0;
   products: any[] = [
     {
-      'prodID': 1,
+      'prodID': 'abc1',
       'prodName': 'Coke',
       'prodDescription': 'HAHAHAHAHA',
       'prodPrice': 80.00,
     },
     {
-      'prodID': 2,
+      'prodID': 'abc2',
       'prodName': 'Sparkle',
       'prodDescription': 'HAHAHAHAHA',
       'prodPrice': 50.00,
     },
     {
-      'prodID': 3,
+      'prodID': 'abc3',
       'prodName': 'Royal',
       'prodDescription': 'HAHAHAHAHA',
       'prodPrice': 80.00,
@@ -31,5 +33,17 @@ export class MenuDisplayComponent implements OnInit {
 
   ngOnInit() {
   }
+  onClickMe() {
+    this.x += 1;
+  }
+  onClickHere(){
+    if(this.x > 0){
+      this.x -= 1;
+    } else {
+      
+    }
+    
+  }
+   
 
 }
